367659355d7beaae41eeb08350f8987329f7d187
EnzoProgram <99368775+EnzoProgram@users.noreply.github.com>
2025-05-28 20:30:11 -0300

    Prueba URL

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/proyecto-final-eas/',
+  //base: '/proyecto-final-eas/',
   build: {
     outDir: 'dist',
     assetsDir: '.',  // Mueve assets a la raíz de 'dist'
