c443f62845f237049d2ef58dfcc86a20431ed300
EnzoProgram <99368775+EnzoProgram@users.noreply.github.com>
2025-05-28 20:32:08 -0300

    PRUEBA MIL

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   //base: '/proyecto-final-eas/',
-  build: {
-    outDir: 'dist',
-    assetsDir: '.',  // Mueve assets a la raíz de 'dist'
-    rollupOptions: {
-      output: {
-        assetFileNames: '[name][extname]',  // Elimina hashes para simplificar
-      },
-    },
-  },
 });
